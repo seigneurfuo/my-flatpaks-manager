3ad5b7f43105aca713895c63d538cdaaf95c351d
seigneurfuo <seigneurfuo@protonmail.com>
2024-05-19 02:47:58 +0200

    Added a tab system with a second tab listing available programs from remote.

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from gi.repository import Gtk, XApp, Flatpak, GLib
 class MainWindow(Gtk.Window):
     def __init__(self):
         super().__init__()
-        self.liststore = Gtk.ListStore(str, str, str)
+        self.liststore_tab0 = Gtk.ListStore(str, str, str)
+        self.liststore_tab1 = Gtk.ListStore(str, str, str, str)
 
         # Create a StatusIcon
         #self.status_icon = XApp.StatusIcon()
@@ -24,7 +25,10 @@ class MainWindow(Gtk.Window):
 
         self._init_ui()
         self._init_events()
-        self._fill_treeview()
+
+    def _init_events(self) -> None:
+        self.connect("destroy", Gtk.main_quit)
+        self.notebook.connect('switch-page', self._on_notebook_page_switched)
 
     def _init_ui(self) -> None:
         self.set_default_size(800, 600)
@@ -32,48 +36,85 @@ class MainWindow(Gtk.Window):
         self.set_title("MyFlatpaksManager - 0.1")
         self.set_position(Gtk.WindowPosition.CENTER)
 
-        label = Gtk.Label(label="Hello, XApp!")
-        #self.add(label)
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
 
+        self._init_ui_tab0()
+        self._init_ui_tab1()
+
+    def _init_ui_tab0(self):
+        box = Gtk.Box()
         vbox = Gtk.VBox()
+        box.add(vbox)
+        self.notebook.append_page(box, Gtk.Label(label="Installed"))
 
         # scrolled_window
-        self.scrolled_window = Gtk.ScrolledWindow()
-        self.scrolled_window.set_vexpand(True)
-        self.scrolled_window.set_hexpand(True)
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_vexpand(True)
+        scrolled_window.set_hexpand(True)
+        vbox.pack_start(scrolled_window, True, True, 0)
 
-        self.treeview = Gtk.TreeView(model=self.liststore)
+        button = Gtk.Button(label="Launch")
+        button.connect("clicked", self.on_launch_button_clicked)
+        vbox.pack_start(button, False, False, 0)
+
+        treeview = Gtk.TreeView(model=self.liststore_tab0)
+        self.treeview_tab0 = treeview
+        scrolled_window.add(treeview)
 
         # Tableau
         # Création d'une colonne texte pour le nom
         column_text = Gtk.TreeViewColumn("Name", Gtk.CellRendererText(), text=0)
-        self.treeview.append_column(column_text)
+        treeview.append_column(column_text)
 
         column_text = Gtk.TreeViewColumn("Version", Gtk.CellRendererText(), text=1)
-        self.treeview.append_column(column_text)
+        treeview.append_column(column_text)
 
         column_text = Gtk.TreeViewColumn("Type", Gtk.CellRendererText(), text=2)
-        self.treeview.append_column(column_text)
+        treeview.append_column(column_text)
 
-        self.scrolled_window.add(self.treeview)
+    def _init_ui_tab1(self):
+        box = Gtk.Box()
+        vbox = Gtk.VBox()
+        box.add(vbox)
+        self.notebook.append_page(box, Gtk.Label(label="Avaiable"))
 
-        self.button = Gtk.Button(label="Launch")
-        self.button.connect("clicked", self.on_launch_button_clicked)
+        # scrolled_window
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_vexpand(True)
+        scrolled_window.set_hexpand(True)
+        vbox.pack_start(scrolled_window, True, True, 0)
 
-        vbox.pack_start(self.scrolled_window, True, True, 0)
-        vbox.pack_start(self.button, False, False, 0)
+        button = Gtk.Button(label="Install")
+        button.connect("clicked", self.on_install_button_clicked)
+        vbox.pack_start(button, False, False, 0)
 
-        self.add(vbox)
+        treeview = Gtk.TreeView(model=self.liststore_tab1)
+        self.treeview_tab1 = treeview
 
-    def _init_events(self) -> None:
-        self.connect("destroy", Gtk.main_quit)
+        scrolled_window.add(treeview)
 
-    def _fill_treeview(self) -> None:
+        # Tableau
+        # Création d'une colonne texte pour le nom
+        column_text = Gtk.TreeViewColumn("Name", Gtk.CellRendererText(), text=0)
+        treeview.append_column(column_text)
+
+        column_text = Gtk.TreeViewColumn("Branch", Gtk.CellRendererText(), text=1)
+        treeview.append_column(column_text)
+
+        column_text = Gtk.TreeViewColumn("Arch", Gtk.CellRendererText(), text=2)
+        treeview.append_column(column_text)
+
+        column_text = Gtk.TreeViewColumn("Remote", Gtk.CellRendererText(), text=3)
+        treeview.append_column(column_text)
+
+
+    def _fill_treeview_tab0(self) -> None:
         installation = Flatpak.Installation.new_system()
-        installed = Flatpak.Installation.list_installed_refs(installation)
-        for package in installed:
+        refs = Flatpak.Installation.list_installed_refs(installation)
+        for ref in refs:
             # Kind
-            kind = package.get_kind()
+            kind = ref.get_kind()
             if kind == Flatpak.RefKind.APP:
                 kind_str = "Application"
             elif kind == Flatpak.RefKind.RUNTIME:
@@ -81,19 +122,76 @@ class MainWindow(Gtk.Window):
             else:
                 kind_str = "?"
 
-            data = [package.get_name(), package.get_appdata_version(), kind_str]
-            self.liststore.append(data)
+            data = [ref.get_name(), ref.get_appdata_version(), kind_str]
+            self.liststore_tab0.append(data)
+
+    def _fill_treeview_tab1(self):
+        installation = Flatpak.Installation.new_system()
+        remotes = installation.list_remotes()
+
+        for remote in remotes:
+            if remote.get_disabled():
+                continue
+
+            refs = installation.list_remote_refs_sync(remote.get_name(), None)
+            for ref in refs:
+                kind_str = ref.get_kind()
+                if kind_str != Flatpak.RefKind.APP:
+                    continue
+
+                data = [ref.get_name(), ref.get_arch(), ref.get_branch(), remote.get_name()]
+                self.liststore_tab1.append(data)
+
+    def _on_notebook_page_switched(self, notebook, tab, index):
+        if index == 0:
+            self._fill_treeview_tab0()
+        elif index == 1:
+            self._fill_treeview_tab1()
 
     def on_launch_button_clicked(self, widget) -> None:
-        selection = self.treeview.get_selection()
+        selection = self.treeview_tab0.get_selection()
         model, treeiter = selection.get_selected()
 
         if not treeiter:
             return
 
         app_id = model[treeiter][0]
+
         cmd = ['flatpak', 'run', app_id]
         subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+
+    def on_install_button_clicked(self, widget) -> None:
+        selection = self.treeview_tab1.get_selection()
+        model, treeiter = selection.get_selected()
+
+        if not treeiter:
+            return
+
+        app_id = model[treeiter][0]
+        branch = model[treeiter][1]
+        arch = model[treeiter][2]
+        remote_name = model[treeiter][3]
+
+        ref_name = f"app/{app_id}/{arch}/{branch}"
+
+        installation = Flatpak.Installation.new_system()
+        transaction = Flatpak.Transaction.new_for_installation(installation)
+        result = transaction.add_install(remote_name, ref_name)
+
+        def on_progress(transaction, progress, user_data):
+            print(f"Progress : {progress * 100:.2f}%")
+
+        transaction.connect("new-operation", on_progress, None)
+
+        try:
+            result = transaction.run()
+            if result == Flatpak.TransactionResult.DONE:
+                print(f"Installdone for{ref_name}")
+            else:
+                print(f"Error while installing: {ref_name}.")
+        except GLib.Error as e:
+            print(f"Error: {ref_name} : {e.message}")
+
 
 def main():
     app = MainWindow()
